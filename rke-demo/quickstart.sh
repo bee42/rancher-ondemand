eaeccc55604ab83b220dcd5b30937722288b956a
cfrein <frein@gmx.de>
2017-12-30 17:42:22 +0100

    some needed commands were commented out in quickstart.sh

--- rke-demo/quickstart.sh
+++ rke-demo/quickstart.sh
@@ -2,9 +2,9 @@
 
 TF_PLAN=dockernodes.tfplan
 
-#terraform validate || exit 1
-#terraform plan -out $TF_PLAN|| exit 2
-#terraform apply $TF_PLAN || exit 3
+terraform validate || exit 1
+terraform plan -out $TF_PLAN|| exit 2
+terraform apply $TF_PLAN || exit 3
 ./create_config.py || exit 4
 rke up || exit 5
 echo Kubernetes-Cluster is running.
